01ee32b5f189f144ad7fb062cec335cff65a7311
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 22:11:01 -0300

    switch

--- App.tsx
+++ App.tsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
 import EntradaDeTexto from "./components/EntradaDeTexto";
 import Input from "./components/EntradaDeTexto";
 import "./styles/global.css"
-import { Text, View, TextInput, TouchableOpacity, Image, Pressable } from "react-native";
- 
+import { Text, View, TextInput, TouchableOpacity, Image, Pressable, Switch } from "react-native";
+
+
+
+
+
 export default function App() {
+const [isEnabled, setIsEnabled] = useState(false);
+const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
+ 
     <View className="flex-1 items-center justify-center bg-[#ffe] first:bg-[url('./assets/fade.png')] bg-top bg-repeat-x px-4">
-      <Image
+
+     <Image
         source={require("./assets/react-icon.png")}
         className="w-40 h-40 rounded-full border-4 border-white opacity-70 mb-10"
       />
-      <Text className="text-2xl text-red-800 font-normal m-1 text-center">
+      <Text className="text-4xl text-red-800 font-extrabold m-1 text-center">
       Bem vindo ao Chan da 3ESPV
       </Text>
-      <EntradaDeTexto/>
+    <View>
+     <Switch
+     className="flex flex-row-reverse"
+     onValueChange={toggleSwitch}
+     value={isEnabled}
+     />
+      <EntradaDeTexto login={isEnabled ? 'Seu Numero' : 'Seu Email'}/>
+    </View>
       <Pressable className="bg-transparent">
-        <Text className="text-red-800 font-normal underline m-1 text-center">Esqueci minha senha</Text>
+        <Text className="text-red-800 font-normal text-2xl underline m-1 text-center">Esqueci minha senha</Text>
       </Pressable>
 
-      <Pressable className="bg-red-800 rounded-lg px-6 py-3 m-4">
+      <Pressable className="bg-red-800 rounded-lg  px-6 py-3 m-4">
         <Text className="text-[#fafafa] text-2xl font-semibold m-1 text-center">SING UP</Text>
       </Pressable>
     </View>
--- components/EntradaDeTexto.tsx
+++ components/EntradaDeTexto.tsx
@@ -1,12 +1,12 @@
 import { TextInput } from "react-native"
 import "../styles/global.css"
 
-export default function EntradaDeTexto(){
+export default function EntradaDeTexto(props){
   return(    
     <>
       <TextInput
         className="w-96 border text-xl border-black rounded-lg px-4 py-5 mb-4 bg-[#fafafa] text-black"
-        placeholder="Seu RM" 
+        placeholder={props.login} 
       />  
 
       <TextInput
